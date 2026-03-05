be8c3464f73c3d26335cccc1d5fa6abec641f24d
w-mcilhagga <william.mcilhagga@gmail.com>
2026-03-05 12:46:45 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name="mymodule", version="0.0", author="me", py_modules=["mymodule"])
+setup(name="mymodule", version="0.1", author="me", py_modules=["mymodule"])
